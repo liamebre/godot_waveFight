23eff9d5d631478f15fa263fc41f291c8539e947
liamebre <164887882+liamebre@users.noreply.github.com>
2025-10-06 17:32:53 -0400

    dash

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,19 +1,15 @@
 extends CharacterBody2D
 @export var speed = 400 # How fast the player will move (pixels/sec).
 var screen_size # Size of the game window.
-var dash = 200
 
 func _ready():
 	screen_size = get_viewport_rect().size
 	
 func get_input():
-
 	var input_dir = Input.get_vector("move_left", "move_right", "move_up", "move_down")
+	velocity = input_dir * speed
 	if Input.is_action_just_pressed("dash"):
-		velocity = input_dir * speed + dash
-	else:
-		velocity = input_dir * speed
-
+		pass
 func _physics_process(delta):
 	get_input()
 	move_and_collide(velocity * delta)
