f3dcedd9567399403da5b4360d5c7cf660c5bf39
liamebre <164887882+liamebre@users.noreply.github.com>
2025-10-07 13:38:57 -0400

    map and dash
    i got the dash working and made a map for the player to move around

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,18 +1,25 @@
 extends CharacterBody2D
 @export var speed = 400 # How fast the player will move (pixels/sec).
 var screen_size # Size of the game window.
+var dashSpeed = speed * 3
+var tt = 0.0
+var lastDash = 0.0
 
 func _ready():
 	screen_size = get_viewport_rect().size
 	
-func get_input():
-	var input_dir = Input.get_vector("move_left", "move_right", "move_up", "move_down")
-	velocity = input_dir * speed
-	if Input.is_action_just_pressed("dash"):
-		pass
 func _physics_process(delta):
+	tt+= delta
 	get_input()
 	move_and_collide(velocity * delta)
 	
+func get_input():
+	var input_dir = Input.get_vector("move_left", "move_right", "move_up", "move_down")
+	if Input.is_action_just_pressed("dash")&&tt - lastDash > 0.50:
+		$dashTimer.start()
+		lastDash = tt #adds a pause inbetween dashes 
+		speed = dashSpeed#stops stacking the dash multiplier
+	velocity = input_dir * speed
 
-	
+func _on_dash_timer_timeout() -> void:
+	speed = 400
