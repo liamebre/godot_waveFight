397c8b59a82abc05d07a0757dac4017069db40f5
liamebre <164887882+liamebre@users.noreply.github.com>
2025-10-10 16:43:20 -0400

    enemies follow
    the bad guys now follow the player

--- scripts/enemy.gd
+++ scripts/enemy.gd
@@ -1,13 +1,16 @@
 extends RigidBody2D
+@onready var player: CharacterBody2D = $"../Player"
 
 func _ready() -> void:
-
 	var mob_types = Array($AnimatedSprite2D.sprite_frames.get_animation_names())
 	$AnimatedSprite2D.animation = mob_types.pick_random()
-	print( mob_types.pick_random())
 	$AnimatedSprite2D.play()
-	print("new mob")
-	
 func _on_visible_onscreen_notifier_2d_screen_exited():
 	queue_free()
+	
+func _physics_process(_delta):
+	var motion = Vector2()
+	position += (player.position - position)/50
+	motion += position.direction_to(player.position)
+	move_and_collide(motion)
 	
