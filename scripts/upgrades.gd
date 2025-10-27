4f6526830b92229e4995b7a3a27ca0e59dc5f5b2
liamebre <164887882+liamebre@users.noreply.github.com>
2025-10-27 13:29:33 -0400

    upgrades
    they kinda have powers know not really tho but they pause the game properly

--- scripts/upgrades.gd
+++ scripts/upgrades.gd
@@ -6,5 +6,12 @@ func _ready() -> void:
 	get_tree().paused = true
 	
 func _on_button_pressed() -> void:
+	if is_in_group("1"):
+		player.health += 10
+		print(player.health)
+	if is_in_group("2"):
+		player.speed+=100
+	if is_in_group("3"):
+		player.sword.damage += 5
 	get_tree().paused = false
 	queue_free()
